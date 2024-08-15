ddb4ba344e17118004f9e56e526bbed2ee546531
brian200508 <36002167+brian200508@users.noreply.github.com>
2024-08-15 11:39:48 +0000

    fix NDK path - include version dir 25.2.9519653

--- dev.sh
+++ dev.sh
@@ -72,7 +72,7 @@ main() {
           git clean -dfX
           git checkout -f HEAD
           (cd ..; rm -rf .pc; QUILT_PATCHES=patches/node-src quilt push -a -f)
-          $USERRUN PATH=/vscode-build/hostbin:$PATH CC_host=gcc CXX_host=g++ LINK_host=g++ ./android-configure /usr/lib/android-sdk/ndk/ $ANDROID_BUILD_API_VERSION $NODE_CONFIGURE_NAME
+          $USERRUN PATH=/vscode-build/hostbin:$PATH CC_host=gcc CXX_host=g++ LINK_host=g++ ./android-configure /usr/lib/android-sdk/ndk/25.2.9519653/ $ANDROID_BUILD_API_VERSION $NODE_CONFIGURE_NAME
           NODE_MAKE_CUSTOM_LDFLAGS=
           if [[ "$ANDROID_ARCH" == "x86" ]]; then
             NODE_MAKE_CUSTOM_LDFLAGS=-latomic
@@ -145,7 +145,7 @@ main() {
         fi
         rm -rf cs-$ANDROID_ARCH.tgz libc++_shared.so node
         cp node-src/out/Release/node ./
-        cp /usr/lib/android-sdk/ndk/sources/cxx-stl/llvm-libc++/libs/$ARCH_NAME/libc++_shared.so ./libc++_shared.so
+        cp /usr/lib/android-sdk/ndk/25.2.9519653/sources/cxx-stl/llvm-libc++/libs/$ARCH_NAME/libc++_shared.so ./libc++_shared.so
         VERSION_SUFFIX=
         if [[ -f patch_version ]]; then
           VERSION_SUFFIX="-p$(cat patch_version)"
