a5014652381d029e3176da8060861efc0a6b93b1
brian200508 <36002167+brian200508@users.noreply.github.com>
2026-04-30 14:22:44 +0200

    fix build: argon2

--- dev.sh
+++ dev.sh
@@ -199,6 +199,7 @@ main() {
             }
             rm -rf release release-standalone node_modules
             export NODE_PATH=/usr/lib/node_modules
+            npm install -g argon2
             npm install -g @mapbox/node-pre-gyp node-addon-api
             $USERRUN mv -f package-lock.json.origbk package-lock.json || true
             $NPM_BIN ci
